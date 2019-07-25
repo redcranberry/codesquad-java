8bd05b7f7385d84c08ea0247eee1a1f65c8eb7d9
Jungeun Kim <jungeun.kim@ericsson.com>
2019-07-25 17:44:53 +0900

    Implement find max days of month

--- src/Calandar.java
+++ src/Calandar.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 
 public class Calandar {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Canlendar");
+        //Put numbers then will printout the selected months largest day
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input month number");
+        int month = scanner.nextInt();
+
+        int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        System.out.printf("%d month has %dday\n", month, maxDays[month - 1]);
+        scanner.close();
     }
 }
